28fd24725ee92b88424cb62318e3daacbc377208
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-09-11 12:09:33 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 400px;
+  padding: 12px;
+  border-radius: 8px;
+  background-color: #dfeff5;
+  border: 3px solid #e88606;
+  list-style: none;
+`;
+
+export const Item = styled.li`
+  background-color: #fff;
+  border: 2px solid #dfeff5;
+  padding: 6px;
+  border-radius: 8px;
+  margin-bottom: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+`;
+
+export const Button = styled.button`
+  padding: 2px 8px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 8px 8px;
+  border: 3px solid #e88606;
+  color: #e88606;
+  background-color: #e8e8e8;
+  &:hover,
+  &:focus {
+    color: #fff;
+    background-color: #e88606;
+    cursor: pointer;
+  }
+`;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FilterCont, Label, Input } from './Filter.styled';
+import { getFilter, setFilter } from 'redux/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
+function Filter() {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+  const onChange = e => {
+    dispatch(setFilter(e.target.value));
+  };
+  
+  return (
+    <FilterCont>
+      <Label>
+        Find contacts by name or phone number
+        <Input type="text" value={filter} onChange={onChange} />
+      </Label>
+    </FilterCont>
+  );
+}
+
+export default Filter;
